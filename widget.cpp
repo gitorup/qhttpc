814c38bc5cad6651d0daf86a241cdeda51d2f9e7
hanfei <superhcq@sina.com>
2018-12-14 09:29:01 +0800

    add title

--- widget.cpp
+++ widget.cpp
@@ -2,12 +2,16 @@
 #include "ui_widget.h"
 
 #include "qnetworkrequest.h"
+#include "qdatetime.h"
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    QDateTime DateTime = QDateTime::currentDateTime();
+    setWindowTitle("QHttp-Tool - " + DateTime.toString("yyyy.MM.dd"));
 }
 
 Widget::~Widget()
